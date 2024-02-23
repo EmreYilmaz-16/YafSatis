177921aa7686356bf20e34d6190add8a323224a5
Emre YILMAZ <emre.yilmaz@hotmail.co.uk>
2024-02-23 12:27:29 +0300

    bilgi

---
+++ Horizon/js/page1.js
@@ -0,0 +1,18 @@
+var ServiceUri="/AddOns/YafSatis/Partner/cfc"
+function getOfferCurrencies(el){
+    $.ajax({
+        url:ServiceUri+"/OfferService.cfc?method=getOfferCurrencies",
+        success:function(returnData){
+            var Obje=JSON.parse(returnData);
+            console.log(Obje);
+            $(el).html("");
+            for(let i=0;i<Obje.length;i++){
+                var option=document.createElement("option");
+                option.value=Obje[i].OFFER_CURRENCY_ID;
+                option.innerText=Obje[i].OFFER_CURRENCY;
+                el.appendChild(option);
+            }
+        }
+    })
+}
+
