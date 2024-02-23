b64895ba9dc0ceb5740969a17d45a3650ab5417b
Emre YILMAZ <emre.yilmaz@hotmail.co.uk>
2024-02-23 13:22:59 +0300

    bilgi

--- Horizon/js/page2.js
+++ Horizon/js/page2.js
@@ -1,4 +1,7 @@
 function openShipList(){
     var CustomerId=document.getElementById("company_id").value;
-    openBoxDraggable("index.cfm?fuseaction=objects.emptypopup_hrz_pbs_smartTools&ListType=getWessels&CustomerId="+CustomerId)
+    if(CustomerId.length>0){
+    openBoxDraggable("index.cfm?fuseaction=objects.emptypopup_hrz_pbs_smartTools&ListType=getWessels&CustomerId="+CustomerId)}else{
+        alert("Müşteri Seçmediniz");
+    }
 }
