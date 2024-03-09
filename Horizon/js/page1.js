fb5246cc8dab12772581bbc74d162f9c2efa686e
Emre YILMAZ <emre.yilmaz@hotmail.co.uk>
2024-03-09 12:04:12 +0300

    teklif kaydetme

--- Horizon/js/page1.js
+++ Horizon/js/page1.js
@@ -1,10 +1,21 @@
+$(document).ready(function(){
+    getDashBoard();
+})
 function getDashBoard(){
     var Uri="/AddOns/YafSatis/partner/cfc/OfferService.cfc?method=getOfferDashBoard"
     $.ajax({
         url:Uri,
         success:function (retDat){
             var Obje=JSON.parse(retDat);
-            console.table(Obje)
+            console.log(Obje)
+            for(let i=0;i<Obje.OFFER_CURRENCY_TOTALS.length;i++){
+                var elem=Obje.OFFER_CURRENCY_TOTALS[i]
+                document.getElementById("OC_"+elem.CURRENCY_ID).innerText=elem.CURRENCY_COUNT
+            }
+            for(let i=0;i<Obje.OFFER_STAGE_TOTALS.length;i++){
+                var elem=Obje.OFFER_STAGE_TOTALS[i]
+                document.getElementById("OC_"+elem.CURRENCY_ID+"_"+elem.PROCESS_ROW_ID).innerText=elem.STAGE_COUNT
+            }
         }
     })
 }
