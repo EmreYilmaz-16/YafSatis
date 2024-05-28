26af3ef7c2bb4eeec7431a584809733ffc2c0849
Emre YILMAZ <emre.yilmaz@hotmail.co.uk>
2024-05-28 17:02:50 +0300

    teklif

--- Horizon/js/sayfa3.js
+++ Horizon/js/sayfa3.js
@@ -1673,6 +1673,14 @@ function TedarikYaz() {
     );
     td.setAttribute("style", "color:#fb6b5b");
     td.appendChild(span);
+    var span = document.createElement("span");
+    span.innerText = AComp.PIDS.length;
+    span.setAttribute(
+      "style",
+      "    float: right;margin-right: 10px;  background: #20a30a;  color: white;  padding: 1px 8px 1px 8px;  border-radius: 50%;"
+    );
+    td.setAttribute("style", "color:#fb6b5b");
+    td.appendChild(span);
     tr.appendChild(td);
     //   tr.setAttribute("style","background: #e1e1e170;")
     tr.setAttribute("onclick", "$('#tr_" + AComp.COMPANY_ID + "').toggle()");
