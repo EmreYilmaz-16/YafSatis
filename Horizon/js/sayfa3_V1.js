461210420f3a0e8cfe53083113270c05f3dd6aed
Emre YILMAZ <emre.yilmaz@hotmail.co.uk>
2024-07-05 11:51:41 +0300

    GÜNCELLEME

--- Horizon/js/sayfa3_V1.js
+++ Horizon/js/sayfa3_V1.js
@@ -2226,3 +2226,8 @@ function SessionKontrolPbs() {
     alert(PSTR);
   }
 }
+
+function urlencode (str) {
+	str = (str+'').toString();
+	return encodeURIComponent(str).replace(/!/g, '%21').replace(/'/g, '%27').replace(/\(/g, '%28').replace(/\)/g, '%29').replace(/\*/g, '%2A').replace(/%20/g, '+');
+}
