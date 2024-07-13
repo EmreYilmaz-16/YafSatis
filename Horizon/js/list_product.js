2d92ccde1f44cead0c5290d591c5a4eeeb93bce2
Emre YILMAZ <emre.yilmaz@hotmail.co.uk>
2024-07-13 09:55:03 +0300

    GÜNCELLEME

--- Horizon/js/list_product.js
+++ Horizon/js/list_product.js
@@ -1,3 +1,11 @@
+$(document).ready(function () {
+    var e = document.getElementById("PRODUCT_CAT");
+    // var e1 = document.getElementById("MONEY");
+    // var e2 = document.getElementById("PRIORITY");
+    //paraBirimleri = wrk_safe_query("getMoneyList", "dsn");
+    getCats(e);
+    //get_consumer("", "");
+  });
 
 function getCatProperties(cat_id) {
     AjaxPageLoad(
@@ -7,4 +15,20 @@ function getCatProperties(cat_id) {
         1,
         "Yükleniyor"
       );
+  }
+  function getCats(el) {
+    $.ajax({
+      url: ServiceUri + "/ProductService.cfc?method=getCats",
+      success: function (returnData) {
+        var Obje = JSON.parse(returnData);
+        // console.log(Obje);
+        $(el).html("");
+        for (let i = 0; i < Obje.length; i++) {
+          var option = document.createElement("option");
+          option.value = Obje[i].PRODUCT_CATID;
+          option.innerText = Obje[i].PRODUCT_CAT;
+          el.appendChild(option);
+        }
+      },
+    });
   }
